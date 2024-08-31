a6ead13f05d0159be295f30557a52534604cc61e
황유빈 <97329194+ppinppini@users.noreply.github.com>
2024-08-31 22:18:16 +0900

    [level 2] Title: 피보나치 수, Time: 6.26 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,10 +1,9 @@
-function solution(n) {
-    let fibo = [0, 1];
-    const rest = 1234567;
-
-    for (let i = 2; i <= n; i++) {
-        fibo[i] = (fibo[i - 2] + fibo[i - 1]) % rest;
+function solution (n){
+    let fibo=[0,1];
+    let rest = 1234567;
+    
+    for(let i =2; i<=n; i++){
+        fibo[i]= (fibo[i-2]+fibo[i-1]) %rest
     }
-
-    return fibo[n];
+    return fibo[n]
 }
